312437436051488170c144c61759f9e9be3884b3
Ahmed Ibrahim <ib153507@gmail.com>
2021-05-14 21:01:22 +0200

    Add the views for posts

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,12 @@
+from rest_framework import generics 
 
-
+from .models import Post
 from .serializers import PostSerializer
 
-class 
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostDetail(RetriveUpdateDostroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
